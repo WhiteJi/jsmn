3790ee84efbfbf36145b296b3e969aa1a36e7b6f
Jiho <21700748@handong.edu>
2018-04-15 00:49:15 +0900

    add example folder

--- example/myjson.c
+++ example/myjson.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../jsmn.h"
-#define DEBUG_MODE
+
 /*
  * A small example of jsmn parsing when JSON structure is known and number of
  * tokens is predictable.
@@ -29,10 +29,10 @@ int main() {
 	jsmn_init(&p);
 	r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t)/sizeof(t[0]));
 
-	#ifdef DEBUG_MODE
-	printf("MAX_TOKEN NUM : 128\n");
+#ifdef DEBUG_MODE
+	printf("MAX_TOKEN NUM : 234\n");
 	printf("Token's num %d \n",r);
-	#endif //토큰수
+#endif //토큰수
 //디버그에요~
 
 	if (r < 0) {
@@ -50,46 +50,58 @@ int main() {
 	for (i = 1; i < r; i++) {
 		if (jsoneq(JSON_STRING, &t[i], "user") == 0) {
 			/* We may use strndup() to fetch string value */
-			#ifdef DEBUG_MODE
+	#ifdef DEBUG_MODE
 			printf("%d  %d\n",t[i+1].start,t[i+1].end);
 			printf("[%d]",i+1);
-			#endif
-			printf("- User: %.*s\n", t[i+1].end-t[i+1].start,
+	#endif
+
+			printf("- User: %.*s", t[i+1].end-t[i+1].start,
 					JSON_STRING + t[i+1].start);
+
+						printf("   parent:%d \n", t[i+1].parent);//parent
+
 			i++;
 		} else if (jsoneq(JSON_STRING, &t[i], "admin") == 0) {
 			/* We may additionally check if the value is either "true" or "false" */
-			#ifdef DEBUG_MODE
+
+	#ifdef DEBUG_MODE
 			printf("%d  %d\n",t[i+1].start,t[i+1].end);
 			printf("[%d]",i+1);
-			#endif
-			printf("- Admin: %.*s\n", t[i+1].end-t[i+1].start,
+	#endif
+			printf("- Admin: %.*s", t[i+1].end-t[i+1].start,
 					JSON_STRING + t[i+1].start);
+						printf("   parent:%d \n", t[i+1].parent);//parent
 			i++;
 		} else if (jsoneq(JSON_STRING, &t[i], "uid") == 0) {
 			/* We may want to do strtol() here to get numeric value */
-			#ifdef DEBUG_MODE
+	#ifdef DEBUG_MODE
 			printf("%d  %d\n",t[i+1].start,t[i+1].end);
 			printf("[%d]",i+1);
-			#endif
-			printf("- UID: %.*s\n", t[i+1].end-t[i+1].start,
+	#endif
+			printf("- UID: %.*s", t[i+1].end-t[i+1].start,
 					JSON_STRING + t[i+1].start);
+						printf("   parent:%d \n", t[i+1].parent);//parent
 			i++;
 		} else if (jsoneq(JSON_STRING, &t[i], "groups") == 0) {
 			int j;
-			#ifdef DEBUG_MODE
+
+	#ifdef DEBUG_MODE
 			printf("%d  %d\n",t[i+1].start,t[i+1].end);
-			#endif
-			printf("- Groups:\n");
+	#endif
+
+			printf("- Groups:");
+				printf("   parent:%d \n", t[i].parent);//parent
+
 			if (t[i+1].type != JSMN_ARRAY) {
 				continue; /* We expect groups to be an array of strings */
 			}
 			for (j = 0; j < t[i+1].size; j++) {
 				jsmntok_t *g = &t[i+j+2];
-				#ifdef DEBUG_MODE
+	#ifdef DEBUG_MODE
 				printf("[%d]",i+1+(j+1));
-				#endif
-				printf("  * %.*s\n", g->end - g->start, JSON_STRING + g->start);
+	#endif
+				printf("  * %.*s", g->end - g->start, JSON_STRING + g->start);
+				printf("   parent:%d \n", t[j+1].parent);//parent
 			}
 			i += t[i+1].size + 1;
 		} else {
